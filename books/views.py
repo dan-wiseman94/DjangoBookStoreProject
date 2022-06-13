8d16e5fb9b7fe631e6156beee3f6cf9931ed652c
Daniel <dan.wiseman94@gmail.com>
2022-06-13 13:46:34 -0500

    first github commit

---
+++ books/views.py
@@ -0,0 +1,32 @@
+from django.db.models import Q 
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin,  PermissionRequiredMixin
+
+from .models import Book
+
+class BookListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Book
+    context_object_name = "book_list" 
+    template_name = "books/book_list.html"
+    login_url = "account_login"
+    permission_required = "books.special_status"
+
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    model = Book
+    context_object_name = "book"
+    template_name = "books/book_detail.html"
+    login_url = "account_login"
+    permission_required = "books.special_status"
+    queryset = Book.objects.all().prefetch_related('reviews__author',) 
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self): # new
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+        Q(title__icontains=query) | Q(author__icontains=query)
+        )
